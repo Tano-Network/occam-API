1a108793a9a10f95c091605d5ef171da42ae367f
Anupam Bera <91262992+Anupam634@users.noreply.github.com>
2025-05-20 22:15:29 +0530

    Update evm.rs

--- script/src/bin/evm.rs
+++ script/src/bin/evm.rs
@@ -336,7 +336,7 @@ async fn main() -> std::io::Result<()> {
             .service(prove_ltv)
     })
     .workers(4)
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
