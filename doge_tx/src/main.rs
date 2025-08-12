373a99d713236311a7ae27fe087adf1691160583
Anupam Bera <91262992+Anupam634@users.noreply.github.com>
2025-08-12 22:20:36 +0530

    Update main.rs

--- doge_tx/src/main.rs
+++ doge_tx/src/main.rs
@@ -10,7 +10,7 @@ pub fn main() {
     let input: DogeTxInput = sp1_zkvm::io::read();
 
     // Verify recipient address (hardcoded for simplicity)
-    let expected_recipient = "DPGGRKJaKtTkNhc6uodtdyQEyv8RsWxL6H";
+    let expected_recipient = "DHGrS3MYGyKzRVdMNxziTPF7QXvaYoEndA";
     assert_eq!(input.recipient_address, expected_recipient, "Recipient address mismatch");
 
     // Hash the sender address
@@ -25,4 +25,5 @@ pub fn main() {
 
     let bytes = PublicValuesDogeTx::abi_encode(&public_values);
     sp1_zkvm::io::commit_slice(&bytes);
+
 }
