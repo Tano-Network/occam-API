9ea57679773c4aa25445d6a38230210b28147de2
Anupam Bera <91262992+Anupam634@users.noreply.github.com>
2025-08-13 20:51:27 +0530

    Update build.rs

--- script/build.rs
+++ script/build.rs
@@ -1,10 +1,10 @@
 use sp1_build::build_program_with_args;
 
 fn main() {
-    build_program_with_args("../program", Default::default());
     build_program_with_args("../doge_tx", Default::default());
 
     
     
 
 }
+
