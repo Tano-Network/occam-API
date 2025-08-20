5aad8baadc194222af21ef85f757dc2eb4cfd913
Ritam Biswas <biswasritam335@gmail.com>
2025-08-20 21:10:43 +0530

    working

--- script/src/bin/evm.rs
+++ script/src/bin/evm.rs
@@ -229,14 +229,14 @@ async fn fetch_doge_tx(tx_hash: &str) -> Result<BlockchairTx, Box<dyn Error>> {
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     setup_logger();
-    println!("Starting DeFi SP1 proof server on http://localhost:4001");
+    println!("Starting DeFi SP1 proof server on http://localhost:4000");
 
     HttpServer::new(|| {
         App::new()
             .service(prove_doge_transaction)
     })
     .workers(1)
-    .bind(("0.0.0.0", 4001))?
+    .bind(("0.0.0.0", 4000))?
     .run()
     .await
 }
